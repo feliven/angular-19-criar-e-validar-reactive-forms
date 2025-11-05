2554e551b5b0aa95b7009698e2e763e00d17d1fd
FVS <feliven@gmail.com>
2025-11-05 12:17:46 -0300

    salva dados inseridos na área "Crie seu cadastro"

--- src/app/pages/dados-pessoais-form/dados-pessoais-form.component.ts
+++ src/app/pages/dados-pessoais-form/dados-pessoais-form.component.ts
@@ -1,7 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ButtonComponent } from '../../shared/components/button/button.component';
+import { Router } from '@angular/router';
+import { CadastroService } from '../../shared/services/cadastro.service';
+
+// interface DadosFormularioInterface {
+//   nomeCompleto: string;
+//   estado: string;
+//   cidade: string;
+//   email: string;
+//   senha: string;
+//   repitaSenha: string;
+// }
 
 @Component({
   selector: 'app-dados-pessoais-form',
@@ -10,10 +26,78 @@ import { ButtonComponent } from '../../shared/components/button/button.component
   templateUrl: './dados-pessoais-form.component.html',
   styleUrls: ['./dados-pessoais-form.component.scss'],
 })
-export class DadosPessoaisFormComponent {
+export class DadosPessoaisFormComponent implements OnInit {
   dadosPessoaisForm!: FormGroup;
 
-  onAnterior(): void {}
+  estados = [
+    { sigla: 'AC', nome: 'Acre' },
+    { sigla: 'AL', nome: 'Alagoas' },
+    { sigla: 'AP', nome: 'Amapá' },
+    { sigla: 'AM', nome: 'Amazonas' },
+    { sigla: 'BA', nome: 'Bahia' },
+    { sigla: 'CE', nome: 'Ceará' },
+    { sigla: 'DF', nome: 'Distrito Federal' },
+    { sigla: 'ES', nome: 'Espírito Santo' },
+    { sigla: 'GO', nome: 'Goiás' },
+    { sigla: 'MA', nome: 'Maranhão' },
+    { sigla: 'MT', nome: 'Mato Grosso' },
+    { sigla: 'MS', nome: 'Mato Grosso do Sul' },
+    { sigla: 'MG', nome: 'Minas Gerais' },
+    { sigla: 'PA', nome: 'Pará' },
+    { sigla: 'PB', nome: 'Paraíba' },
+    { sigla: 'PR', nome: 'Paraná' },
+    { sigla: 'PE', nome: 'Pernambuco' },
+    { sigla: 'PI', nome: 'Piauí' },
+    { sigla: 'RJ', nome: 'Rio de Janeiro' },
+    { sigla: 'RN', nome: 'Rio Grande do Norte' },
+    { sigla: 'RS', nome: 'Rio Grande do Sul' },
+    { sigla: 'RO', nome: 'Rondônia' },
+    { sigla: 'RR', nome: 'Roraima' },
+    { sigla: 'SC', nome: 'Santa Catarina' },
+    { sigla: 'SP', nome: 'São Paulo' },
+    { sigla: 'SE', nome: 'Sergipe' },
+    { sigla: 'TO', nome: 'Tocantins' },
+  ];
 
-  onProximo(): void {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private cadastroService: CadastroService
+  ) {}
+
+  ngOnInit(): void {
+    this.dadosPessoaisForm = this.formBuilder.group({
+      nomeCompleto: ['', Validators.required],
+      estado: ['', Validators.required],
+      cidade: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(8)]],
+      repitaSenha: ['', Validators.required],
+    });
+  }
+
+  onAnterior(): void {
+    this.salvarDadosInseridos();
+    this.router.navigate(['/cadastro/area-atuacao']);
+  }
+
+  onProximo(): void {
+    if (this.dadosPessoaisForm.valid) {
+      this.salvarDadosInseridos();
+      this.router.navigate(['/cadastro/confirmacao']);
+    } else {
+      this.dadosPessoaisForm.markAllAsTouched();
+    }
+  }
+
+  private salvarDadosInseridos(): void {
+    const dadosInseridos = this.dadosPessoaisForm.value;
+    this.cadastroService.atualizarDadosCadastro({
+      nomeCompleto: dadosInseridos.nomeCompleto,
+      estado: dadosInseridos.estado,
+      cidade: dadosInseridos.cidade,
+      email: dadosInseridos.email,
+      senha: dadosInseridos.senha,
+    });
+  }
 }
